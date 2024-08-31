6ee22173ca26368020348db80804d2a1c4213387
Maanish07 <manishkr88089@gmail.com>
2024-08-31 16:17:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello node");
+});
+
+const port = process.env.PORT | 3333;
+app.listen(port, () => {
+  console.log("app is listining  on port :", port);
+});
